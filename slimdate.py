1115236cd92728833805e471c4621a80a920892d
tonsh <tonshlee@gmail.com>
2013-12-05 11:46:44 +0800

    Add SlimDatetime class

---
+++ slimdate.py
@@ -0,0 +1,235 @@
+# coding=utf-8
+
+import datetime
+
+class SlimDatetime(object):
+
+    def __init__(self, date_time):
+        self.value = date_time
+
+        self._date = None
+        self._time = None
+        self._year = None
+        self._month = None
+        self._day = None
+        self._hour = None
+        self._minute = None
+        self._second = None
+        self._weekday = None
+
+    def __repr__(self):
+        return '<SlimDatetime class>'
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+    def __le__(self, other):
+        return self.value <= other.value
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+    def __ne__(self, other):
+        return self.value != other.value
+
+    def __gt__(self, other):
+        return self.value > other.value
+
+    def __ge__(self, other):
+        return self.value >= other.value
+
+    @property
+    def date(self):
+        if not self._date:
+            self._date = self.value.date()
+
+        return self._date
+
+    @property
+    def time(self):
+        if not self._time:
+            self._time = self.value.time()
+
+        return self._time
+
+    @property
+    def year(self):
+        if not self._year:
+            self._year = self.value.year
+
+        return self._year
+
+    @property
+    def month(self):
+        if not self._month:
+            self._month = self.value.month
+
+        return self._month
+
+    @property
+    def day(self):
+        if not self._day:
+            self._day = self.value.day
+
+        return self._day
+
+    @property
+    def hour(self):
+        if not self._hour:
+            self._hour = self.value.hour
+
+        return self._hour
+
+    @property
+    def minute(self):
+        if not self._minute:
+            self._minute = self.value.minute
+
+        return self._minute
+
+    @property
+    def second(self):
+        if not self._second:
+            self._second = self.value.second
+
+        return self._second
+
+    @property
+    def weekday(self):
+        if not self._weekday:
+            self._weekday = self.value.weekday() + 1
+
+        return self._weekday
+
+    def beginning_of_day(self):
+        return datetime.datetime.combine(self.date, self.time.min)
+
+    def end_of_day(self):
+        return datetime.datetime.combine(self.date, self.time.max)
+
+    def beginning_of_hour(self):
+        v_time = datetime.time(self.hour)
+        return datetime.datetime.combine(self.date, v_time)
+
+    def end_of_hour(self):
+        time_max = self.time.max
+        v_time = datetime.time(self.hour,
+                                time_max.minute,
+                                time_max.second,
+                                time_max.microsecond)
+
+        return datetime.datetime.combine(self.date, v_time)
+
+    def beginning_of_minute(self):
+        v_time = datetime.time(self.hour, self.minute)
+        return datetime.datetime.combine(self.date, v_time)
+
+    def end_of_minute(self):
+        time_max = self.time.max
+        v_time = datetime.time(self.hour, self.minute,
+                                time_max.second,
+                                time_max.microsecond)
+
+        return datetime.datetime.combine(self.date, v_time)
+
+    def last_week(self, weekday=None):
+        days = 7
+        if weekday:
+            days = (7 + self.weekday) - weekday
+
+        return self.beginning_of_day() - datetime.timedelta(days=days)
+
+    def this_week(self, weekday=None):
+        days = 0
+        if weekday:
+            days = weekday - self.weekday
+
+        return self.beginning_of_day() + datetime.timedelta(days=days)
+
+    def next_week(self, weekday=None):
+        days = 7
+        if weekday:
+            days = 7 - self.weekday + weekday
+
+        return self.beginning_of_day() + datetime.timedelta(days=days)
+
+    def last_month(self, monthday=None):
+        day = monthday if monthday else self.day 
+        year, month, day = self.change_month(self.year, self.month - 1, day)
+
+        return self.beginning_of_day().replace(year=year, month=month, day=day)
+
+    def this_month(self, monthday=None):
+        day = monthday if monthday else self.day 
+        year, month, day = self.change_month(self.year, self.month, day)
+
+        return self.beginning_of_day().replace(day=day)
+
+    def next_month(self, monthday=None):
+        day = monthday if monthday else self.day 
+        year, month, day = self.change_month(self.year, self.month + 1, day)
+
+        return self.beginning_of_day().replace(year=year, month=month, day=day)
+
+    @staticmethod
+    def change_month(year, month, day):
+        if month < 1:
+            year -= 1
+            month = 12 - abs(month)
+
+        if month > 12:
+            year += 1
+            month = abs(month) - 12
+
+        n_year = year
+        n_month = month + 1
+        if n_month > 12:
+            n_year += 1
+            n_month = 1
+        next_month = datetime.datetime(year=n_year, month=n_month, day=1)
+
+        this_month = next_month - datetime.timedelta(days=1)
+
+        # day 不能超过当月最大天数，若大于最大天数则默认为最大天数
+        day = min(day, this_month.day)
+
+        return (this_month.year, this_month.month, day)
+
+
+def str2datetime(date_str, fmt=None):
+    if isinstance(date_str, datetime.datetime):
+        return date_str
+    
+    if date_str:
+        if fmt:
+            return datetime.datetime.strptime(date_str, fmt)
+
+        fmts = list([
+            '%Y-%m-%d',
+            '%Y-%m-%d %H:%M:%S',
+            '%m/%d/%Y',
+            '%y%m%d',
+            '%Y%m%d',
+            '%y-%m-%d',
+            '%Y-%m-%d %H:%M:%S:%f',
+            '%a %b %d %X +0800 %Y',
+        ])
+    
+        for fmt in fmts:
+            try:
+                return datetime.datetime.strptime(date_str, fmt)
+            except ValueError:
+                continue
+
+    return None
+
+
+def datetime_from_timestamp(timestamp):
+    dt = datetime.datetime.fromtimestamp(timestamp)
+    return SlimDatetime(dt)
+
+
+def datetime_from_string(date_str, fmt=None):
+    dt = str2datetime(date_str, fmt)
+    return SlimDatetime(dt)
+
---
+++ slimdate_test.py
@@ -0,0 +1,397 @@
+# coding=utf-8
+
+import datetime
+import time
+import unittest
+
+from slimdate import (SlimDatetime,
+                    str2datetime,
+                    datetime_from_timestamp,
+                    datetime_from_string)
+
+
+class SlimDateTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.date_time = datetime.datetime(2013, 12, 04, 20, 43, 11)
+        self.slim_date = SlimDatetime(self.date_time)
+
+    def tearDown(self):
+        self.date_time = None
+        self.slim_date = None
+
+    def test_init(self):
+        self.assertEqual(self.slim_date.value, self.date_time)
+
+    def test_lt(self):
+        slim_date_2 = SlimDatetime(datetime.datetime.now())
+        self.assertTrue(self.slim_date < slim_date_2)
+
+    def test_le(self):
+        slim_date_2 = SlimDatetime(datetime.datetime(2013, 12, 04, 20, 43, 11))
+        self.assertTrue(self.slim_date <= slim_date_2)
+
+    def test_eq(self):
+        slim_date_2 = SlimDatetime(datetime.datetime(2013, 12, 04, 20, 43, 11))
+        self.assertTrue(self.slim_date == slim_date_2)
+
+    def test_gt(self):
+        slim_date_2 = SlimDatetime(datetime.datetime(2013, 11, 11))
+        self.assertTrue(self.slim_date > slim_date_2)
+
+    def test_ge(self):
+        slim_date_2 = SlimDatetime(datetime.datetime(2013, 11, 11))
+        self.assertTrue(self.slim_date >= slim_date_2)
+
+    def test_date(self):
+        self.assertIsNone(self.slim_date._date)
+        self.assertEqual(datetime.date(2013, 12, 04), self.slim_date.date)
+        self.assertIsNotNone(self.slim_date._date)
+
+    def test_time(self):
+        self.assertIsNone(self.slim_date._time)
+        self.assertEqual(datetime.time(20, 43, 11), self.slim_date.time)
+        self.assertIsNotNone(self.slim_date._time)
+
+    def test_year(self):
+        self.assertIsNone(self.slim_date._year)
+        self.assertEqual(2013, self.slim_date.year)
+        self.assertIsNotNone(self.slim_date._year)
+
+    def test_month(self):
+        self.assertIsNone(self.slim_date._month)
+        self.assertEqual(12, self.slim_date.month)
+        self.assertIsNotNone(self.slim_date._month)
+
+    def test_day(self):
+        self.assertIsNone(self.slim_date._day)
+        self.assertEqual(4, self.slim_date.day)
+        self.assertIsNotNone(self.slim_date._day)
+
+    def test_hour(self):
+        self.assertIsNone(self.slim_date._hour)
+        self.assertEqual(20, self.slim_date.hour)
+        self.assertIsNotNone(self.slim_date._hour)
+
+    def test_minute(self):
+        self.assertIsNone(self.slim_date._minute)
+        self.assertEqual(43, self.slim_date.minute)
+        self.assertIsNotNone(self.slim_date._minute)
+
+    def test_second(self):
+        self.assertIsNone(self.slim_date._second)
+        self.assertEqual(11, self.slim_date.second)
+        self.assertIsNotNone(self.slim_date._second)
+
+    def test_weekday(self):
+        self.assertIsNone(self.slim_date._weekday)
+        self.assertEqual(3, self.slim_date.weekday)
+        self.assertIsNotNone(self.slim_date._weekday)
+
+    def test_beginning_of_day(self):
+        expect = datetime.datetime(2013, 12, 4)
+        self.assertEqual(expect, self.slim_date.beginning_of_day())
+
+    def test_end_of_day(self):
+        expect = datetime.datetime(2013, 12, 4, 23, 59, 59, 999999)
+        self.assertEqual(expect, self.slim_date.end_of_day())
+
+    def test_beginning_of_hour(self):
+        expect = datetime.datetime(2013, 12, 4, 20)
+        self.assertEqual(expect, self.slim_date.beginning_of_hour())
+
+    def test_end_of_hour(self):
+        expect = datetime.datetime(2013, 12, 4, 20, 59, 59, 999999)
+        self.assertEqual(expect, self.slim_date.end_of_hour())
+
+    def test_beginning_of_minute(self):
+        expect = datetime.datetime(2013, 12, 4, 20, 43)
+        self.assertEqual(expect, self.slim_date.beginning_of_minute())
+
+    def test_end_of_minute(self):
+        expect = datetime.datetime(2013, 12, 4, 20, 43, 59, 999999)
+        self.assertEqual(expect, self.slim_date.end_of_minute())
+
+    def test_lastweek(self):
+        # 默认
+        expect = datetime.datetime(2013, 11, 27)
+        self.assertEqual(expect, self.slim_date.last_week())
+
+        # 上周一
+        expect = datetime.datetime(2013, 11, 25)
+        self.assertEqual(expect, self.slim_date.last_week(1))
+
+        # 上周二
+        expect = datetime.datetime(2013, 11, 26)
+        self.assertEqual(expect, self.slim_date.last_week(2))
+
+        # 上周三
+        expect = datetime.datetime(2013, 11, 27)
+        self.assertEqual(expect, self.slim_date.last_week(3))
+
+        # 上周四
+        expect = datetime.datetime(2013, 11, 28)
+        self.assertEqual(expect, self.slim_date.last_week(4))
+
+        # 上周五
+        expect = datetime.datetime(2013, 11, 29)
+        self.assertEqual(expect, self.slim_date.last_week(5))
+
+        # 上周六
+        expect = datetime.datetime(2013, 11, 30)
+        self.assertEqual(expect, self.slim_date.last_week(6))
+
+        # 上周日
+        expect = datetime.datetime(2013, 12, 1)
+        self.assertEqual(expect, self.slim_date.last_week(7))
+
+    def test_this_week(self):
+        # 默认
+        expect = datetime.datetime(2013, 12, 4)
+        self.assertEqual(expect, self.slim_date.this_week())
+
+        # 本周一
+        expect = datetime.datetime(2013, 12, 2)
+        self.assertEqual(expect, self.slim_date.this_week(1))
+
+        # 本周二
+        expect = datetime.datetime(2013, 12, 3)
+        self.assertEqual(expect, self.slim_date.this_week(2))
+
+        # 本周三
+        expect = datetime.datetime(2013, 12, 4)
+        self.assertEqual(expect, self.slim_date.this_week(3))
+
+        # 本周四
+        expect = datetime.datetime(2013, 12, 5)
+        self.assertEqual(expect, self.slim_date.this_week(4))
+
+        # 本周五
+        expect = datetime.datetime(2013, 12, 6)
+        self.assertEqual(expect, self.slim_date.this_week(5))
+
+        # 本周六
+        expect = datetime.datetime(2013, 12, 7)
+        self.assertEqual(expect, self.slim_date.this_week(6))
+
+        # 本周日
+        expect = datetime.datetime(2013, 12, 8)
+        self.assertEqual(expect, self.slim_date.this_week(7))
+
+    def test_next_week(self):
+        # 默认
+        expect = datetime.datetime(2013, 12, 11)
+        self.assertEqual(expect, self.slim_date.next_week())
+
+        # 下周一
+        expect = datetime.datetime(2013, 12, 9)
+        self.assertEqual(expect, self.slim_date.next_week(1))
+
+        # 下周二
+        expect = datetime.datetime(2013, 12, 10)
+        self.assertEqual(expect, self.slim_date.next_week(2))
+
+        # 下周三
+        expect = datetime.datetime(2013, 12, 11)
+        self.assertEqual(expect, self.slim_date.next_week(3))
+
+        # 下周四
+        expect = datetime.datetime(2013, 12, 12)
+        self.assertEqual(expect, self.slim_date.next_week(4))
+
+        # 下周五
+        expect = datetime.datetime(2013, 12, 13)
+        self.assertEqual(expect, self.slim_date.next_week(5))
+
+        # 下周六
+        expect = datetime.datetime(2013, 12, 14)
+        self.assertEqual(expect, self.slim_date.next_week(6))
+
+        # 下周日
+        expect = datetime.datetime(2013, 12, 15)
+        self.assertEqual(expect, self.slim_date.next_week(7))
+
+    def test_last_month(self):
+        # 默认
+        expect = datetime.datetime(2013, 11, 4)
+        self.assertEqual(expect, self.slim_date.last_month())
+
+        # 上月1号
+        expect = datetime.datetime(2013, 11, 1)
+        self.assertEqual(expect, self.slim_date.last_month(1))
+
+        # 上月4号
+        expect = datetime.datetime(2013, 11, 4)
+        self.assertEqual(expect, self.slim_date.last_month(4))
+
+        # 上月15号
+        expect = datetime.datetime(2013, 11, 15)
+        self.assertEqual(expect, self.slim_date.last_month(15))
+
+        # 上月30号
+        expect = datetime.datetime(2013, 11, 30)
+        self.assertEqual(expect, self.slim_date.last_month(30))
+
+        # 上月31号, 11月没有31会返回11月的最后一天
+        expect = datetime.datetime(2013, 11, 30)
+        self.assertEqual(expect, self.slim_date.last_month(31))
+
+        # 一月的上个月
+        slim_date_2 = SlimDatetime(datetime.datetime(2014, 1, 4))
+        expect = datetime.datetime(2013, 12, 4)
+        self.assertEqual(expect, slim_date_2.last_month(4))
+
+    def test_this_month(self):
+        # 默认
+        expect = datetime.datetime(2013, 12, 4)
+        self.assertEqual(expect, self.slim_date.this_month())
+
+        # 本月1号
+        expect = datetime.datetime(2013, 12, 1)
+        self.assertEqual(expect, self.slim_date.this_month(1))
+
+        # 本月4号
+        expect = datetime.datetime(2013, 12, 4)
+        self.assertEqual(expect, self.slim_date.this_month(4))
+
+        # 本月15号
+        expect = datetime.datetime(2013, 12, 15)
+        self.assertEqual(expect, self.slim_date.this_month(15))
+
+        # 本月31号
+        expect = datetime.datetime(2013, 12, 31)
+        self.assertEqual(expect, self.slim_date.this_month(31))
+
+        # 平年二月最后一天
+        slim_date_2 = SlimDatetime(datetime.datetime(2013, 2, 1))
+        expect = datetime.datetime(2013, 2, 28)
+        self.assertEqual(expect, slim_date_2.this_month(31))
+
+        # 闰年二月最后一天
+        slim_date_2 = SlimDatetime(datetime.datetime(2012, 2, 1))
+        expect = datetime.datetime(2012, 2, 29)
+        self.assertEqual(expect, slim_date_2.this_month(31))
+
+    def test_next_month(self):
+        # 默认
+        expect = datetime.datetime(2014, 1, 4)
+        self.assertEqual(expect, self.slim_date.next_month())
+
+        # 下月1号
+        expect = datetime.datetime(2014, 1, 1)
+        self.assertEqual(expect, self.slim_date.next_month(1))
+
+        # 下月4号
+        expect = datetime.datetime(2014, 1, 4)
+        self.assertEqual(expect, self.slim_date.next_month(4))
+
+        # 下月15号
+        expect = datetime.datetime(2014, 1, 15)
+        self.assertEqual(expect, self.slim_date.next_month(15))
+
+        # 下月31号
+        expect = datetime.datetime(2014, 1, 31)
+        self.assertEqual(expect, self.slim_date.next_month(31))
+
+        # 平年二月最后一天
+        slim_date_2 = SlimDatetime(datetime.datetime(2013, 1, 1))
+        expect = datetime.datetime(2013, 2, 28)
+        self.assertEqual(expect, slim_date_2.next_month(31))
+
+        # 闰年二月最后一天
+        slim_date_2 = SlimDatetime(datetime.datetime(2012, 1, 1))
+        expect = datetime.datetime(2012, 2, 29)
+        self.assertEqual(expect, slim_date_2.next_month(31))
+
+    def test_change_month(self):
+        ymd_tuple = SlimDatetime.change_month(2013, 12, 31)
+        self.assertEqual((2013, 12, 31), ymd_tuple)
+
+        # 1月的前1个月
+        ymd_tuple = SlimDatetime.change_month(2013, 1 - 1, 32)
+        self.assertEqual((2012, 12, 31), ymd_tuple)
+        # 1月的前4个月
+        ymd_tuple = SlimDatetime.change_month(2013, -3, 32)
+        self.assertEqual((2012, 9, 30), ymd_tuple)
+
+        # 12月的下1个月
+        ymd_tuple = SlimDatetime.change_month(2013, 13, 3)
+        self.assertEqual((2014, 1, 3), ymd_tuple)
+        # 12月的下4个月
+        ymd_tuple = SlimDatetime.change_month(2013, 16, 4)
+        self.assertEqual((2014, 4, 4), ymd_tuple)
+
+        # 平年2月的最后一天
+        ymd_tuple = SlimDatetime.change_month(2013, 2, 31)
+        self.assertEqual((2013, 2, 28), ymd_tuple)
+
+        # 闰年二月最后一天
+        ymd_tuple = SlimDatetime.change_month(2012, 2, 31)
+        self.assertEqual((2012, 2, 29), ymd_tuple)
+
+
+class GetSlimDatetimeTestCase(unittest.TestCase):
+
+    def test_str2datetime(self):
+        # 参数为日期类型，直接返回
+        a_date = datetime.datetime.now()
+        self.assertEqual(a_date, str2datetime(a_date))
+
+        # None
+        self.assertIsNone(str2datetime(None))
+        
+        #'%Y-%m-%d'
+        a_date = datetime.datetime(2013, 9, 29)
+        self.assertEqual(a_date, str2datetime('2013-09-29'))
+        
+        #'%Y-%m-%d %H:%M:%S'
+        a_date = datetime.datetime(2013, 9, 29, 18, 5, 23)
+        self.assertEqual(a_date, str2datetime('2013-09-29 18:05:23'))
+        
+        #'%m/%d/%Y'
+        a_date = datetime.datetime(2013, 9, 7)
+        self.assertEqual(a_date, str2datetime('09/07/2013'))
+
+        #'%Y%m%d'
+        a_date = datetime.datetime(2013, 9, 29)
+        self.assertEqual(a_date, str2datetime('20130929'))
+        
+        #'%y%m%d'
+        a_date = datetime.datetime(2013, 9, 29)
+        self.assertEqual(a_date, str2datetime('130929'))
+        
+        #'%y-%m-%d'
+        a_date = datetime.datetime(2013, 9, 29)
+        self.assertEqual(a_date, str2datetime('13-09-29'))
+        
+        #'%a %b %d %X +0800 %Y'
+        a_date = datetime.datetime(2013, 5, 31, 17, 46, 55)
+        self.assertEqual(a_date, str2datetime('Tue May 31 17:46:55 +0800 2013'))
+        
+        #'%Y-%m-%d %H:%M:%S:%f'
+        a_date = datetime.datetime(2013, 9, 29, 18, 5, 23)
+        self.assertEqual(a_date, str2datetime('2013-09-29 18:05:23:000'))
+
+    def test_str2datetime_by_fmt(self):
+        # None
+        self.assertIsNone(str2datetime(None, '%Y-%m-%d'))
+        
+        #'%Y-%m-%d'
+        a_date = datetime.datetime(2013, 9, 29)
+        self.assertEqual(a_date, str2datetime('2013-09-29', '%Y-%m-%d'))
+        
+        #'%Y-%m-%d %H:%M:%S'
+        a_date = datetime.datetime(2013, 9, 29, 18, 5, 23)
+        self.assertEqual(a_date, str2datetime('2013-09-29 18:05:23'))
+
+    def test_datetime_from_string(self):
+        a_date = SlimDatetime(datetime.datetime(2013, 9, 7))
+        self.assertEqual(a_date, datetime_from_string('09/07/2013'))
+
+    def test_datetime_from_timestamp(self):
+        dt = datetime.datetime(2013, 9, 7)
+        a_date = SlimDatetime(dt)
+
+        dt_timestamp = time.mktime(dt.timetuple())
+        self.assertEqual(a_date, datetime_from_timestamp(dt_timestamp))
+
